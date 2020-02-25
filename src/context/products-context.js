380e54709ed1902b1cfcc959bb557dd170248dec
LaupWing <loc_strict@hotmail.com>
2020-02-25 14:24:38 +0100

    522

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -1,36 +1,55 @@
 import React from 'react'
 
-export const ProductsContext = React.createContext({products:[]})
+export const ProductsContext = React.createContext({
+    products:[],
+    toggleFav: (id)=>{}
+})
 
 export default props =>{
     const [products, setProducts] = React.useState([
-    {
-      id: 'p1',
-      title: 'Red Scarf',
-      description: 'A pretty red scarf.',
-      isFavorite: false
-    },
-    {
-      id: 'p2',
-      title: 'Blue T-Shirt',
-      description: 'A pretty blue t-shirt.',
-      isFavorite: false
-    },
-    {
-      id: 'p3',
-      title: 'Green Trousers',
-      description: 'A pair of lightly green trousers.',
-      isFavorite: false
-    },
-    {
-      id: 'p4',
-      title: 'Orange Hat',
-      description: 'Street style! An orange hat.',
-      isFavorite: false
+        {
+            id: 'p1',
+            title: 'Red Scarf',
+            description: 'A pretty red scarf.',
+            isFavorite: false
+        },
+        {
+            id: 'p2',
+            title: 'Blue T-Shirt',
+            description: 'A pretty blue t-shirt.',
+            isFavorite: false
+        },
+        {
+            id: 'p3',
+            title: 'Green Trousers',
+            description: 'A pair of lightly green trousers.',
+            isFavorite: false
+        },
+        {
+            id: 'p4',
+            title: 'Orange Hat',
+            description: 'Street style! An orange hat.',
+            isFavorite: false
+        }
+    ])
+
+    const toggleProducts = (productId) =>{
+        setProducts(curr =>{
+            const prodIndex = curr.findIndex(
+                p => p.id === productId
+              );
+              const newFavStatus = !curr[prodIndex].isFavorite;
+              const updatedProducts = [...curr];
+              updatedProducts[prodIndex] = {
+                ...curr[prodIndex],
+                isFavorite: newFavStatus
+              };
+              return updatedProducts
+        })
     }
-  ])
+
     return(
-        <ProductsContext.Provider value={{products}}>
+        <ProductsContext.Provider value={{products, toggleFav: toggleProducts}}>
             {props.children}
         </ProductsContext.Provider>
     )
